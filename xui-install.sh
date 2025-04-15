b9ecc53085c9711ef0038f814254554b3a667442
ProjectV2V <158727991+ProjectV2V@users.noreply.github.com>
2025-04-15 14:28:05 +0330

    Create xui-install.sh

---
+++ xui-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
+yes n | bash <(curl -s https://raw.githubusercontent.com/Adymob123/V2RaySetup/refs/heads/main/setup-v2ray.sh)
+sleep 10
+systemctl stop x-ui
+wget -O /etc/x-ui/x-ui.db https://raw.githubusercontent.com/ProjectV2V/Assets/main/x-ui.db
+chown root:root /etc/x-ui/x-ui.db
+chmod 600 /etc/x-ui/x-ui.db
+wget -O /root/cert.crt https://raw.githubusercontent.com/ProjectV2V/Assets/main/cert.crt
+wget -O /root/private.key https://raw.githubusercontent.com/ProjectV2V/Assets/main/private.key
+chmod 600 /root/*.key
+systemctl restart x-ui
+echo '✅ x-ui auto install done!' > /root/script_status.txt
